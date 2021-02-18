1b937e83e2549a1958bed538b7cab47bac2f571e
Gabriel Mota <gabrielfmota134@gmail.com>
2021-02-18 01:01:05 -0300

    Add script to all levels
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- assets/scripts/Level.gd
+++ assets/scripts/Level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+export (int) var level_num
+export (PackedScene) var next_scene
 var fire_done : bool = false
 var water_done : bool = false
 onready var door : Area2D = $Door
@@ -9,7 +11,13 @@ func _ready():
 	door.connect("body_entered", self, "entered_door")
 	door.connect("body_exited", self, "exit_door")
 	fade.connect("end_fade", self, "fade_ended")
-	fade.fade_in(1)
+	
+	if get_node("Water") != null:
+		get_node("Water").connect("body_entered", self, "on_water_body_entered")
+	if get_node("Fire") != null:
+		get_node("Fire").connect("body_entered", self, "on_fire_body_entered")
+	
+	fade.fade_in(level_num)
 
 func entered_door(body):
 	if body.is_in_group("WaterHero"):
@@ -18,6 +26,7 @@ func entered_door(body):
 		fire_done = true
 	
 	if water_done and fire_done:
+		door.open()
 		fade.fade_out()
 
 func exit_door(body):
@@ -27,4 +36,15 @@ func exit_door(body):
 		fire_done = false
 
 func fade_ended():
-	print("FOI")
+	get_tree().change_scene(next_scene.get_path())
+
+func on_water_body_entered(body):
+	if body.is_in_group("FireHero"):
+		lose()
+
+func on_fire_body_entered(body):
+	if body.is_in_group("WaterHero"):
+		lose()
+
+func lose():
+	print("perdeu")
