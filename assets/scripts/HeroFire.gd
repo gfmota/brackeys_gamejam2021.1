eb793c578f47c28ba5b21299f0900c4735c474c8
Gabriel Mota <gabrielfmota134@gmail.com>
2021-02-18 00:45:37 -0300

    Add jumping animation
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- assets/scripts/HeroFire.gd
+++ assets/scripts/HeroFire.gd
@@ -8,9 +8,6 @@ var friction : bool
 var motion : Vector2
 onready var sprite : AnimatedSprite = $Sprite
 
-func _ready():
-	sprite.play("default")
-
 func _physics_process(delta):
 	friction = false
 	motion.y += GRAVITY
@@ -25,11 +22,16 @@ func _physics_process(delta):
 		friction = true
 	# Jumping
 	if is_on_floor():
+		sprite.play("default")
 		if Input.is_action_just_pressed("fire_up"):
 			motion.y += JUMP_HEIGHT
 		if friction:
 			motion.x = lerp(motion.x, 0, 0.2)
 	else:
+		if motion.y < 0:
+			sprite.play("jumping")
+		if motion.y > 0:
+			sprite.play("falling")
 		if friction:
 			motion.x = lerp(motion.x, 0, 0.2)
 	motion = move_and_slide(motion, Vector2(0, -1))
--- assets/scripts/HeroWater.gd
+++ assets/scripts/HeroWater.gd
@@ -6,10 +6,11 @@ const JUMP_HEIGHT : int = -550
 const MAX_SPEED : int = 200
 var friction : bool
 var motion : Vector2
+onready var jump_animation_timer : Timer = $Jump_anim
 onready var sprite : AnimatedSprite = $Sprite
 
 func _ready():
-	sprite.play("default")
+	jump_animation_timer.connect("timeout", self, "jump_animation_ended")
 
 func _physics_process(delta):
 	friction = false
@@ -25,11 +26,20 @@ func _physics_process(delta):
 		friction = true
 	# Jumping
 	if is_on_floor():
+		sprite.play("default")
 		if Input.is_action_just_pressed("water_up"):
 			motion.y += JUMP_HEIGHT
+			sprite.play("jump")
+			jump_animation_timer.start()
 		if friction:
 			motion.x = lerp(motion.x, 0, 0.2)
 	else:
+		if motion.y > 0:
+			sprite.play("falling")
 		if friction:
 			motion.x = lerp(motion.x, 0, 0.2)
 	motion = move_and_slide(motion, Vector2(0, -1))
+
+
+func jump_animation_ended():
+	sprite.play("in_air")
